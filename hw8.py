cc87171471c86eff67fa8d0843e80c48b9694d64
leejun0113 <ijunyeob101@gmail.com>
2025-05-11 16:23:45 +0900

    Add py File [hw8.py]

---
+++ hw8.py
@@ -0,0 +1,31 @@
+def buy(shopping_bag):
+    print('[구입]')
+    item_name = input('상품명? ')
+
+    if item_name == '':
+        return False
+    
+    item_amount = int(input('수량은? '))
+    shopping_bag[item_name] = item_amount
+    print(f'장바구니에 {item_name} {item_amount}개가 담겼습니다. \n')
+    return True
+    
+def show(shopping_bag):
+    print(f'\n>>> 장바구니 보기: {shopping_bag}')
+
+def find(shopping_bag):
+    print('\n[검색]')
+    search_item = input('장바구니에서 확인하고자 하는 상품은? ')
+    
+    if search_item in shopping_bag:
+        find_item = shopping_bag[search_item]
+        print(f'{search_item}은(는) {find_item}개 담겨 있습니다.')
+    else:
+        print(f'{search_item}은(는) 장바구니에 없습니다.')
+
+shopping_bag = {}
+while True:
+    if buy(shopping_bag) == False:
+        break
+show(shopping_bag)
+find(shopping_bag)
