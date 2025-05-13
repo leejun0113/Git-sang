7c68b6d1d85daa5f125225e6d4a6e81e854bd9d6
leejun0113 <ijunyeob101@gmail.com>
2025-05-13 14:46:19 +0900

    Add py File [hw8.py]

--- hw8.py
+++ hw8.py
@@ -16,6 +16,7 @@ def show(shopping_bag):
 def find(shopping_bag):
     print('\n[검색]')
     search_item = input('장바구니에서 확인하고자 하는 상품은? ')
+    
     if search_item in shopping_bag:
         find_item = shopping_bag[search_item]
         print(f'{search_item}은(는) {find_item}개 담겨 있습니다.')
